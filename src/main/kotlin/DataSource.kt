004f4578927bcb9e8ae1df7c85fc4fc8cdc29651
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-06-28 16:27:17 +0300

    Comments added for objects and some new words added

--- src/main/kotlin/DataSource.kt
+++ src/main/kotlin/DataSource.kt
@@ -1,4 +1,11 @@
+/**
+ * [POINTS_PER_WORD] constant holds max number of points that user can earn if they guess a word
+ */
 const val POINTS_PER_WORD = 50
+
+/**
+ * [DataSource] object holds a set of words for game
+ */
 object DataSource {
     val wordsSet = setOf(
         "umbrella",
@@ -17,5 +24,23 @@ object DataSource {
         "narrow",
         "push-off",
         "reassuring",
+        "entrance",
+        "error",
+        "escape",
+        "mutiny",
+        "mutineers",
+        "salami slicing",
+        "defacing",
+        "denial-of-service attack",
+        "malware",
+        "white hat hacker",
+        "black hat hacker",
+        "outrageous",
+        "unfold",
+        "pull a muscle",
+        "pull a knife",
+        "rail",
+        "bezel",
+        "floodwaters",
     )
 }
