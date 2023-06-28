4629130dde6d9511dd74d52b9803b6a0308eb1b8
Nikolay Ponomarenko <fallinkotlin@gmail.com>
2023-06-28 16:28:57 +0300

    Comments for objects and functions were added. The problem with ending the game after a user types "false" to finish the game was fixed. New function was added to display the final message for the user

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ import DataSource.wordsSet
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlin.system.exitProcess
 
 /**
  * [totalScore] defines a user's total score at the end of the game
@@ -9,15 +10,18 @@ import kotlinx.coroutines.runBlocking
 var totalScore = 0
 
 /**
+ * [usedWords] set is used to store already used words in a game and to prevent the repetitions
+ */
+
+var usedWords = mutableSetOf<String>()
+
+/**
  * [main] is a starting point of the app
  */
 
 fun main() = runBlocking {
-    val wordToGuess = wordsSet.random()
-    val unscrambledWord = unscrambleWord(word = wordToGuess)
     introduceToPlayer()
-    displayWordToGuess(word = unscrambledWord)
-    guessWord(wordToGuess = wordToGuess)
+    guessWord()
 }
 
 /**
@@ -25,8 +29,8 @@ fun main() = runBlocking {
  */
 
 fun introduceToPlayer() {
-    println("Welcome to \"Unscramble\". In this game, you have to guess the unscrambled word. \nLet's get started, my baby...")
-    println("By the way, you have only 8 attempts to guess the word. If you do not guess it, you own me your soul...\n(just kidding)")
+    println("Welcome to \"Unscramble\". In this game, you have to guess the unscrambled word.")
+    println("If you do not guess it, you own me your soul...\n(just kidding)")
 }
 
 /**
@@ -52,22 +56,38 @@ fun unscrambleWord(word: String): String {
 }
 
 /**
- * [chooseNewWord] function is just returns a new word that need to be guessed
+ * [randomWord] function is just returns a new word that need to be guessed
  */
-fun chooseNewWord(): String = wordsSet.random()
+fun randomWord(): String = wordsSet.random()
 
 /**
- * At the end of the game, [randomCompliment] function displays a message for a player with the earned score
+ * At the end of the game, [randomCompliment] function displays a message for a player with the earned total score
  */
 
 fun randomCompliment(totalScore: Int): String {
+    val maxEarnedPoints = POINTS_PER_WORD * wordsSet.size
     return when (totalScore) {
         0 -> "Very bad, I'm disappointed!!!!"
         in 50..150 -> "Not bad!"
         in 151..200 -> "Good job!"
         in 201..250 -> "Awesome!"
         in 251..300 -> "Fucking monster. I'm as proud as Punch of you. Keep going"
-        else -> "Stop...Please, stop... You are too good for this game. I should beat my developers' asses to make this game way much harder"
+        in 301..500 -> "Stop...Please, stop... You are too good for this game. I should beat my developers' asses to make this game way much harder"
+        in 501..maxEarnedPoints -> "You guessed all of the words. Cool! I'm astonished and feel proud of you!\n(And yeah, that's everything that I wanted to say because I'm speechless)"
+        else -> "Something went wrong"
+    }
+}
+
+/**
+ * [displayAfterGameMessage] function display the result message for a player after the game ends
+ * @see [randomCompliment]
+ */
+
+fun displayAfterGameMessage(totalScore: Int) {
+    println("You earned $totalScore. ${randomCompliment(totalScore = totalScore)}")
+    println("User's list of guessed words: ")
+    usedWords.forEach { word ->
+        println("----> $word")
     }
 }
 
@@ -75,34 +95,38 @@ fun randomCompliment(totalScore: Int): String {
  * [guessWord] function holds the game logic
  */
 
-suspend fun guessWord(wordToGuess: String): Unit = coroutineScope {
+suspend fun guessWord(wordToGuess: String = randomWord()): Unit = coroutineScope {
+    fun isContinuePlaying(): Boolean {
+        println("Do you want to continue playing? ('true' or 'false')")
+        return readln().toBoolean()
+    }
     while (true) {
-        val userGuess = readln()
-        if (userGuess.equals(wordToGuess, ignoreCase = true)) {
-            totalScore += POINTS_PER_WORD
-            println("Cool, $POINTS_PER_WORD points were earned")
-            fun isContinuePlaying(): Boolean {
-                println("Do you want to continue playing? ('true' or 'false')")
-                return readln().toBoolean()
-            }
-            if (isContinuePlaying()) {
-                val newWordToGuess = chooseNewWord()
-                val unscrambledWord = unscrambleWord(word = newWordToGuess)
-                delay(1000L)
-                displayWordToGuess(word = unscrambledWord)
-                guessWord(wordToGuess = newWordToGuess)
-            } else {
-                val endMessage = """
-                    THE END OF THE GAME!
-                    You earned $totalScore. ${randomCompliment(totalScore = totalScore)}
-                """.trimIndent()
-                println(endMessage)
-            }
+        if (usedWords.contains(wordToGuess)) {
+            guessWord(wordToGuess = randomWord())
         } else {
-            delay(250L)
-            println("No. Go ahead")
-            continue
+            usedWords.add(wordToGuess)
+            delay(340L)
+            displayWordToGuess(word = unscrambleWord(word = wordToGuess))
+            val userGuess = readln()
+            if (userGuess.equals(wordToGuess, ignoreCase = true)) {
+                totalScore += POINTS_PER_WORD
+                println("Cool, $POINTS_PER_WORD points were earned")
+            } else {
+                println("Nope. Try again")
+                continue
+            }
         }
-        break
+
+        if (usedWords.size == wordsSet.size) {
+            displayAfterGameMessage(totalScore = totalScore)
+            exitProcess(0)
+        }
+
+        val answer = isContinuePlaying()
+        if (answer) guessWord()
+        else {
+            displayAfterGameMessage(totalScore = totalScore)
+            exitProcess(0)
+        }
     }
 }
